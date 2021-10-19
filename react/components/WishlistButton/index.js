b246d68d887dc270d1ba2161a4eb7e4f2adc0972
Vishuddhi Bhuta <vishuddhi.bhuta@krishtechnolabs.local>
2021-10-19 18:54:09 +0530

    Initial commit

---
+++ react/components/WishlistButton/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css'
+
+const WishlistButton = ({AddListenButton}) => {
+    return(
+        <div className="wishlist-wrapper">Add to Wishlist <AddListenButton/></div>
+    )
+}
+
+export default WishlistButton 
